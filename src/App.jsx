003bbb33ac8f2d5a4d4591638ef997e285b81e34
naimur8606 <119111473+naimur8606@users.noreply.github.com>
2023-09-14 23:29:24 +0600

    courses-container creat and course added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Cart from './components/Cart'
 import Course from './components/Course'
 
 function App() {
+  const [courses, setCourses] = useState([])
+  useEffect( () => {
+    fetch('./Courses.json')
+      .then(res => res.json())
+      .then(data => setCourses(data))
+  },[])
   
   return (
-    <>
+    <div className='w-11/12 mx-auto'>
       <h1 className='text-4xl my-10 text-center font-bold'>Course Registration</h1>
       <div>
-        <div className="course-container">
-          <Course></Course>
+        <div className="course-container lg:w-[79%] grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {
+            courses.map( course => <Course key={course.id} course={course}></Course>)
+          }
         </div>
         <div>
           <Cart></Cart>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
--- src/Components/Course.jsx
+++ src/Components/Course.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
-const Course = () => {
+const Course = ({course}) => {
+    const {cover, title, credit, description, price} = course;
     return (
-        <div>
-           Naimur 
+        <div className='p-4 bg-[#FFFF] rounded-lg space-y-2'>
+            <img className='w-full h-[160px] rounded-lg' src={cover} alt="" />
+            <h2 className='text-xl font-semibold'>{title}</h2>
+            <p className='text-base text-[#999898]'>{description}</p>
+            <div className='flex justify-between'>
+                <div className='flex  items-center'>
+                    <span className='flex text-3xl mr-2'>$</span>
+                    <p className='text-[18px] text-[#999898]'>Price: {price}</p>
+                </div>
+                <div className='flex items-center'>
+                    <img className='w-6 mr-2' src="./book.png" alt="" />
+                    <p className='text-[18px] text-[#999898]'>Credit: {credit}hr</p>
+                </div>
+            </div>
+            <button className='bg-[#2F80ED] text-white text-xl font-semibold w-full rounded-lg py-2'>Select</button>
         </div>
     );
 };
